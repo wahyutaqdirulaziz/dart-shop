cb55018782cf43bceba0ba3d2a63e3229b7ecc67
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-09-20 17:00:07 +0700

    Consumer widget instead of Provider.of()

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,35 +7,37 @@ import '../screens/product_detail_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          title: Text(product.title, textAlign: TextAlign.center),
-          leading: IconButton(
-            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () => product.toggleFavoriteStatus(),
+    return Consumer<Product>(
+      builder: (_, product, __) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: GridTile(
+            footer: GridTileBar(
+              backgroundColor: Colors.black87,
+              title: Text(product.title, textAlign: TextAlign.center),
+              leading: IconButton(
+                icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+                color: Theme.of(context).accentColor,
+                onPressed: () => product.toggleFavoriteStatus(),
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.shopping_cart),
+                color: Theme.of(context).accentColor,
+                onPressed: () {},
+              ),
+            ),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed(
+                  ProductDetailScreen.routeName,
+                  arguments: product.id,
+                );
+              },
+              child: Image.network(product.imageUrl, fit: BoxFit.cover),
+            ),
           ),
-          trailing: IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
-          child: Image.network(product.imageUrl, fit: BoxFit.cover),
-        ),
-      ),
+        );
+      },
     );
   }
 }
