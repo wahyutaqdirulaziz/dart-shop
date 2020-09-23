d9a2a1b03c97eed1fd2983dbc3cd74c9754350e0
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-09-23 15:16:36 +0700

    TextFormField validator option & form.currentState.validate()

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -14,9 +14,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+
   final _form = GlobalKey<FormState>();
-  final _inputs = {'title': '', 'description': '', 'price': '', 'imageUrl': ''};
-  Product _editedProduct;
+  final _inputs = <String, String>{};
 
   @override
   void initState() {
@@ -36,13 +36,24 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _updateImageUrl() {
     if (!_imageUrlFocusNode.hasFocus) {
+      if (_imageUrlController.text.isEmpty ||
+          (!_imageUrlController.text.startsWith('http') &&
+              !_imageUrlController.text.startsWith('https')) ||
+          (!_imageUrlController.text.endsWith('.png') &&
+              !_imageUrlController.text.endsWith('.jpg') &&
+              !_imageUrlController.text.endsWith('.jpeg'))) {
+        return;
+      }
       setState(() {});
     }
   }
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) return;
+
     _form.currentState.save();
-    _editedProduct = Product(
+    final _editedProduct = Product(
       title: _inputs['title'],
       description: _inputs['description'],
       price: double.parse(_inputs['price']),
@@ -80,6 +91,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_priceFocusNode);
                   },
+                  validator: (value) {
+                    return value.isEmpty ? 'Please provide a value' : null;
+                  },
                   onSaved: (value) => _inputs['title'] = value,
                 ),
                 TextFormField(
@@ -90,6 +104,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_descriptionFocusNode);
                   },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a price';
+                    } else if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    } else if (double.parse(value) <= 0) {
+                      return 'Please enter a number greater than zero';
+                    } else {
+                      return null;
+                    }
+                  },
                   onSaved: (value) => _inputs['price'] = value,
                 ),
                 TextFormField(
@@ -97,6 +122,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   maxLines: 3,
                   keyboardType: TextInputType.multiline,
                   focusNode: _descriptionFocusNode,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a description';
+                    } else if (value.length < 10) {
+                      return 'Should be at least 10 characters long';
+                    } else {
+                      return null;
+                    }
+                  },
                   onSaved: (value) => _inputs['description'] = value,
                 ),
                 Row(
@@ -125,6 +159,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
                         controller: _imageUrlController,
                         focusNode: _imageUrlFocusNode,
                         onFieldSubmitted: (_) => _saveForm(),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter an image URL';
+                          } else if (!value.startsWith('http') &&
+                              !value.startsWith('https')) {
+                            return 'Please enter a valid URL';
+                          } else if (!value.endsWith('.png') &&
+                              !value.endsWith('.jpg') &&
+                              !value.endsWith('.jpeg')) {
+                            return 'Please enter a valid image URL';
+                          } else {
+                            return null;
+                          }
+                        },
                         onSaved: (value) => _inputs['imageUrl'] = value,
                       ),
                     ),
