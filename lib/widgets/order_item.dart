baaf7d9c0eb0c3027bc740d56a19e5820edbb868
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-09-27 16:02:07 +0700

    Animate the expanding card using AnimatedContainer

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -19,51 +19,61 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget._order.totalPrice.toStringAsFixed(2)}'),
-            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget._order.dateTime)),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget._order.products.length * 20.0 + 20, 100),
-              child: ListView(
-                children: widget._order.products.map((product) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.title,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        '${product.quantity} x \$${product.price.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  );
-                }).toList(),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded ? min(widget._order.products.length * 20.0 + 120, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text('\$${widget._order.totalPrice.toStringAsFixed(2)}'),
+                subtitle:
+                    Text(DateFormat('dd/MM/yyyy hh:mm').format(widget._order.dateTime)),
+                trailing: IconButton(
+                  icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  },
+                ),
               ),
-            ),
-        ],
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                  height:
+                      _expanded ? min(widget._order.products.length * 20.0 + 20, 100) : 0,
+                  child: ListView(
+                    children: widget._order.products.map((product) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${product.quantity} x \$${product.price.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
